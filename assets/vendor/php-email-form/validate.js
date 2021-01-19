2f46ce14250a9f856c06db3373fefdb95b007fc7
Nakul Aggarwal <you@example.com>
2021-01-19 16:05:45 +0530

    Contact Form Completed

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -101,25 +101,25 @@ jQuery(document).ready(function($) {
       return false;
     }
     
-    this_form.find('.sent-message').slideUp();
-    this_form.find('.error-message').slideUp();
-    this_form.find('.loading').slideDown();
+    // this_form.find('.sent-message').slideUp();
+    // this_form.find('.error-message').slideUp();
+    // this_form.find('.loading').slideDown();
     
-    $.ajax({
-      type: "POST",
-      url: action,
-      data: str,
-      success: function(msg) {
-        if (msg == 'OK') {
-          this_form.find('.loading').slideUp();
-          this_form.find('.sent-message').slideDown();
-          this_form.find("input:not(input[type=submit]), textarea").val('');
-        } else {
-          this_form.find('.loading').slideUp();
-          this_form.find('.error-message').slideDown().html(msg);
-        }
-      }
-    });
+    // $.ajax({
+    //   type: "POST",
+    //   url: action,
+    //   data: str,
+    //   success: function(msg) {
+    //     if (msg == 'OK') {
+    //       this_form.find('.loading').slideUp();
+    //       this_form.find('.sent-message').slideDown();
+    //       this_form.find("input:not(input[type=submit]), textarea").val('');
+    //     } else {
+    //       this_form.find('.loading').slideUp();
+    //       this_form.find('.error-message').slideDown().html(msg);
+    //     }
+    //   }
+    // });
     return false;
   });
 
