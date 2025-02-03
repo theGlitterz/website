6d09ffe27f457455f2bb5aa026afa69b985af251
theGlitterz <90838033+theGlitterz@users.noreply.github.com>
2025-02-03 01:36:58 +0000

    Delete assets directory

--- assets/js/visiting-card.js
+++
@@ -1,8 +0,0 @@
-let img = document.getElementById("download-image");
-
-function imageDownloader() {
-    let imagePath = img.getAttribute('src');
-    let fileName = "Glitterz Business Card.png"
-    saveAs(imagePath, fileName);
-}
-window.onload = imageDownloader;
